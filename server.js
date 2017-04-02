eeac739a72dd6626e5ffa11a4534a7afa5aa7f43
Joseph W Becher <drazisil@users.noreply.github.com>
2017-04-02 14:46:27 -0400

    Shell config (#76)
    * Added shell config
    
    * Added error handling

--- server.js
+++ server.js
@@ -8,9 +8,18 @@ var dz = require('./src/index.js')
 POST()
 
 function POST () {
-  dz.init(function done () {
-    dz.checkDataFiles(function done () {
-      dz.updateData(function done () {
+  dz.init(function done (err) {
+    if (err) {
+      throw err
+    }
+    dz.checkDataFiles(function done (err) {
+      if (err) {
+        throw err
+      }
+      dz.updateData(function done (err) {
+        if (err) {
+          throw err
+        }
         dz.timerId = setTimeout(dz.updateData, 1800000, function done () { // 30 minutes
         })
         dz.startServer()
